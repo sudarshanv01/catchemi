46421910f72543affd0b1a3c78d1874b3479fd59
Sudarshan Vijay <vijays@fysik.dtu.dk>
2021-11-15 14:32:57 +0100

    Changed file layout

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+""" Defining the setup.py for the poisson package. """
+
+
+def setup_package():
+    """Install the `norskov_newns_anderson` package."""
+    import json
+    from setuptools import setup, find_packages
+
+    filename_setup_json = 'setup.json'
+    filename_description = 'README.md'
+
+    with open(filename_setup_json, 'r') as handle:
+        setup_json = json.load(handle)
+
+    with open(filename_description, 'r') as handle:
+        description = handle.read()
+
+    setup(include_package_data=True,
+          packages=find_packages(),
+          long_description=description,
+          long_description_content_type='text/markdown',
+          **setup_json)
+
+
+if __name__ == '__main__':
+    setup_package()
