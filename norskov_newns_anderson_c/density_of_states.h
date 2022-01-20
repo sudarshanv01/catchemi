54b36ef18a9b698fecb206a039dfe17580260046
Sudarshan Vijay <vijays@fysik.dtu.dk>
2022-01-20 08:14:48 +0100

    Change to fully python implementation

--- norskov_newns_anderson_c/density_of_states.h
+++
@@ -1,2 +0,0 @@
-#include "acb.h"
-void get_density_of_states(acb_t dos, double Vak, double eps, double eps_d, double wd, double eps_a, double Delta0, int prec);
--- norskov_newns_anderson_c/quantities_newns_anderson.h
+++
@@ -1,4 +0,0 @@
-#include <acb.h>
-void get_Delta_semiellipse(acb_t Delta, double Vak_f, double eps_f, double eps_d_f, double wd_f, long prec);
-void get_Lambda_semiellipse(acb_t Lambda, double Vak_f, double eps_f, double eps_d_f, double wd_f, long prec);
-void get_energy_difference(acb_t energy_difference, double eps_f, double eps_a_f, int prec);
