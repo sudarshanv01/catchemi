24bbbddbf02c74582ae36c8ea9ea4e29050e8ef9
Sudarshan Vijay <svijay@lbl.gov>
2023-08-13 15:19:07 +0200

    Add documentation for the base NA class

--- src/catchemi/core/_fixed_dim.py
+++ src/catchemi/core/_fixed_dim.py
@@ -44,6 +44,43 @@ class FixedDimensionCalculation(BaseCalculation):
         spin factor is 1. If False, the spin factor is 2.
     eps_f : float, optional
         Fermi energy. Default is 0.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from catchemi.core import FixedDimensionCalculation
+    >>> eps = np.linspace(-10, 10, 100) # Energy grid from DFT
+    >>> eps = eps.reshape(1, -1) # Reshape to make first dimension input dim
+    >>> pdos = np.abs(np.random.rand(100)) # Projected density of states from DFT
+    >>> pdos = pdos.reshape(1, -1) # Reshape to match the energy grid
+    >>> coupling_sd = np.random.rand(1) # Coupling strength parameter
+    >>> coupling_sd = coupling_sd.reshape(1, -1) # Reshape to match the energy grid
+    >>> eps_a = 0.0 # Adsorbate energy
+    >>> alpha = 1.0 # Orthogonalization parameter
+    >>> beta = 0.2 # Hybridization parameter
+    >>> Delta0 = 0.5 # Constant shift of the normalized projected density of states
+    >>> spin_polarized = False # Whether or not the system is spin polarized
+    >>> eps_f = 0.0 # Fermi energy
+    >>> fixed_dim_calc = FixedDimensionCalculation(
+    ...     eps=eps,
+    ...     pdos=pdos,
+    ...     coupling_sd=coupling_sd,
+    ...     eps_a=eps_a,
+    ...     alpha=alpha,
+    ...     beta=beta,
+    ...     Delta0=Delta0,
+    ...     spin_polarized=spin_polarized,
+    ...     eps_f=eps_f,
+    ... )
+    >>> hybridization_energy = fixed_dim_calc.get_hybridization_energy()
+    >>> orthogonalization_energy = fixed_dim_calc.get_orthogonalization_energy()
+    >>> chemisorption_energy = fixed_dim_calc.get_chemisorption_energy()
+    >>> assert hybridization_energy.shape == (1, 1)
+    >>> assert orthogonalization_energy.shape == (1, 1)
+    >>> assert chemisorption_energy.shape == (1, 1)
+    >>> assert hybridization_energy <= 0
+    >>> assert orthogonalization_energy >= 0
+    >>> assert chemisorption_energy <= 0
     """
 
     eps: npt.ArrayLike
@@ -84,17 +121,55 @@ class FixedDimensionCalculation(BaseCalculation):
         """
         return pdos / np.sum(pdos, axis=1)[:, None]
 
-    def get_Delta(self, Vaksq: npt.ArrayLike, rho_aa: npt.ArrayLike) -> npt.ArrayLike:
-        """Get the Delta parameter."""
-        Delta = self.beta * Vaksq * rho_aa
+    def get_Delta(self, Vaksq: npt.ArrayLike, rho_d: npt.ArrayLike) -> npt.ArrayLike:
+        """Get the Delta parameter.
+
+        The Delta parameter is computed for the d-bands and the sp-bands. The d-bands
+        contribution is given by,
+        .. math::
+            \\Delta_{d} = \\pi V_{ak}^{2} \\rho_{d} + \\Delta_{0}
+        and the total contribution is given by,
+        .. math::
+            \\Delta = \\Delta_{d} + \\Delta_{0}
+        where :math:`\\Delta_{0}` is the contribution from the sp-bands and is
+        assumed constants throughout the energy grid.
+
+        Parameters
+        ----------
+        Vaksq : npt.ArrayLike
+            Coupling strength of the orbital of the adsorbate and the d-orbital
+            of the surface.
+        rho_d : npt.ArrayLike
+            Normalizd projected density of states of the d-orbital of the surface.
+
+        Returns
+        -------
+        npt.ArrayLike
+            Delta parameter.
+        """
+        Delta = np.pi * Vaksq * rho_d
         Delta += self.Delta0
         return Delta
 
     def get_Lambda(self, Delta: npt.ArrayLike) -> npt.ArrayLike:
+        """Get the Lambda parameter.
+
+        The Lambda parameter is simply the Hilbert transform of the Delta parameter.
+
+        Parameters
+        ----------
+        Delta : npt.ArrayLike
+            Delta parameter.
+
+        Returns
+        -------
+        npt.ArrayLike
+            Lambda parameter.
+        """
         Lambda = np.imag(scipy.signal.hilbert(Delta, axis=-1))
         return Lambda
 
-    def get_arctan_integrand(
+    def _get_arctan_integrand(
         self,
         Delta: npt.ArrayLike,
         Lambda: npt.ArrayLike,
@@ -110,11 +185,26 @@ class FixedDimensionCalculation(BaseCalculation):
         return arctan_integrand
 
     def get_hybridization_energy(self) -> npt.ArrayLike:
+        """Get the hybridization energy.
+
+        The hybridization energy is given by,
+        .. math::
+            E_{hyb} = \\frac{1}{\\pi} \\int_{-\\infty}^{\\infty} d\\epsilon \\frac{\\Delta(\\epsilon)}{\\epsilon - \\epsilon_{a} - \\Lambda(\\epsilon)}
+        where :math:`\\Delta(\\epsilon)` is the Delta parameter, :math:`\\epsilon_{a}` is the
+        adsorbate energy level and :math:`\\Lambda(\\epsilon)` is the Lambda parameter all on
+        the energy grid :math:`\\epsilon` for each spin channel.
+        The integral is evaluated using the trapezoidal rule.
+
+        Returns
+        -------
+        npt.ArrayLike
+            Hybridization energy (in eV).
+        """
         Vaksq = self.beta * self.coupling_sd**2
         rho_d = self.normalize_pdos(self.pdos)
         Delta = self.get_Delta(Vaksq, rho_d)
         Lambda = self.get_Lambda(Delta)
-        arctan_integrand = self.get_arctan_integrand(
+        arctan_integrand = self._get_arctan_integrand(
             Delta, Lambda, self.eps, self.eps_a
         )
         arctan_integrand *= self.mask
@@ -132,18 +222,29 @@ class FixedDimensionCalculation(BaseCalculation):
         eps: npt.ArrayLike,
         eps_a: float,
     ) -> npt.ArrayLike:
+        """Get the adsorbate density of states.
+
+        The adsorbate density of states is given by,
+        .. math::
+            \\rho_{aa} = \\frac{1}{\\pi} \\frac{\\Delta}{(\\epsilon - \\epsilon_{a} - \\Lambda)^{2} + \\Delta^{2}}
+        where :math:`\\Delta` is the Delta parameter, :math:`\\epsilon_{a}` is the
+        adsorbate energy level and :math:`\\Lambda` is the Lambda parameter all on
+        the energy grid :math:`\\epsilon` for each spin channel.
+        """
         rho_aa = Delta
         rho_aa /= (eps - eps_a - Lambda) ** 2 + Delta**2
         rho_aa /= np.pi
         return rho_aa
 
-    def get_occupancy(self, rho_aa: npt.ArrayLike, eps: npt.ArrayLike) -> npt.ArrayLike:
+    def _get_occupancy(
+        self, rho_aa: npt.ArrayLike, eps: npt.ArrayLike
+    ) -> npt.ArrayLike:
         na_integrand = rho_aa * self.mask
         na = np.trapz(na_integrand, x=eps, axis=-1)
         na = na.reshape(-1, 1)
         return na
 
-    def get_filling(self, Delta: npt.ArrayLike, eps: npt.ArrayLike) -> npt.ArrayLike:
+    def _get_filling(self, Delta: npt.ArrayLike, eps: npt.ArrayLike) -> npt.ArrayLike:
         f_integrand_numerator = Delta * self.mask
         f_integrand_denominator = Delta
         filling = np.trapz(f_integrand_numerator, x=eps, axis=-1) / np.trapz(
@@ -152,11 +253,26 @@ class FixedDimensionCalculation(BaseCalculation):
         filling = filling.reshape(-1, 1)
         return filling
 
-    def get_overlap(self, Vaksq: npt.ArrayLike, alpha: float) -> npt.ArrayLike:
+    def _get_overlap(self, Vaksq: npt.ArrayLike, alpha: float) -> npt.ArrayLike:
         overlap = -1 * np.sqrt(Vaksq) * alpha
         return overlap
 
     def get_orthogonalization_energy(self) -> npt.ArrayLike:
+        """Get the orthogonalization energy.
+
+        The orthogonalization energy is given by,
+        .. math::
+            E_{ortho} = - (n_{a} + f) \\sqrt{V_{ak}^{2}} \\alpha
+        for the adsorbate density of states :math:`\\rho_{aa}`, the adsorbate energy level
+        :math:`\\epsilon_{a}`, the filling :math:`f`, the occupancy :math:`n_{a}`, the
+        overlap :math:`\\alpha` and the coupling :math:`V_{ak}` in the two-state
+        approximation.
+
+        Returns
+        -------
+        npt.ArrayLike
+            Orthogonalization energy (in eV).
+        """
         Vaksq = self.beta * self.coupling_sd**2
         rho_d = self.normalize_pdos(self.pdos)
         Delta = self.get_Delta(Vaksq, rho_d)
@@ -164,15 +280,28 @@ class FixedDimensionCalculation(BaseCalculation):
         rho_aa = self.get_adsorbate_density_of_states(
             Delta, Lambda, self.eps, self.eps_a
         )
-        occupancy = self.get_occupancy(rho_aa, self.eps)
-        filling = self.get_filling(Delta, self.eps)
-        overlap = self.get_overlap(Vaksq, self.alpha)
+        occupancy = self._get_occupancy(rho_aa, self.eps)
+        filling = self._get_filling(Delta, self.eps)
+        overlap = self._get_overlap(Vaksq, self.alpha)
         orthogonalization_energy = -self.spin_factor * (occupancy + filling)
         orthogonalization_energy *= np.sqrt(Vaksq)
         orthogonalization_energy *= overlap
         return orthogonalization_energy
 
     def get_chemisorption_energy(self) -> npt.ArrayLike:
+        """Get the chemisorption energy.
+
+        The chemisorption energy is given by,
+        .. math::
+            E_{chem} = E_{hyb} + E_{ortho}
+        where :math:`E_{hyb}` is the hybridization energy and :math:`E_{ortho}` is the
+        orthogonalization energy.
+
+        Returns
+        -------
+        npt.ArrayLike
+            Chemisorption energy (in eV).
+        """
         hybridization_energy = self.get_hybridization_energy()
         orthogonalization_energy = self.get_orthogonalization_energy()
         chemisorption_energy = hybridization_energy + orthogonalization_energy
